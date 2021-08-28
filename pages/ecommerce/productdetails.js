a16604f542b1d7fa914ed30f3de1394958a4ebdc
Anonymous <xolpolatovshohjahhon5@gmail.com>
2021-08-28 12:05:26 +0500

    product daetaillls

--- pages/ecommerce/productdetails.js
+++ pages/ecommerce/productdetails.js
@@ -1,5 +1,5 @@
 import Dashboard from "../../containers/Dashboard";
-import ProductDetailes from "../../containers/Ecomerce/ProductDetailes";
+import ProductDetailes from "../../containers/Ecomerce/productDetailes";
 import React from "react";
 
 const ProductDetailis = () => {

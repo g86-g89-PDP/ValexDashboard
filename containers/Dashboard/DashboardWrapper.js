2d8dac5dced1dc8abd76046fb9132e32cac8e776
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2021-08-28 10:42:34 +0500

    DashboardWrapper

--- containers/Dashboard/DashboardWrapper.js
+++ containers/Dashboard/DashboardWrapper.js
@@ -5,7 +5,7 @@ const DashboardWrapper = styled.div`
   .sidebar {
     border-right: 2px solid #333c51 !important;
     flex-shrink: 0;
-    width: 50px;
+    width: 58px;
     overflow: hidden;
     background: #1f2940 !important;
     transition: 0.3s;
@@ -37,7 +37,7 @@ const DashboardWrapper = styled.div`
     .imgLogo {
       position: absolute;
       top: 10px;
-      left: 50px;
+      left: 54px;
     }
 
     nav {

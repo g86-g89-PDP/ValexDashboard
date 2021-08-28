abf6724e51bd88e5a18353658499bc2702322670
khurshid02 <axrorovxurshid39@gmail.com>
2021-08-28 09:24:37 +0500

    change

---
+++ containers/Products/Products.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ProductsWrapper from "./ProductsWrapper";
+
+const Products = () => {
+  return (
+    <ProductsWrapper>
+      <div className="m-1">
+        <div>
+          <div>
+            <p>Ecommerce</p>
+            <p> / Products</p>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </ProductsWrapper>
+  );
+};
+
+export default Products;
---
+++ containers/Products/ProductsWrapper.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const ProductsWrapper = styled.div``;
+
+export default ProductsWrapper;

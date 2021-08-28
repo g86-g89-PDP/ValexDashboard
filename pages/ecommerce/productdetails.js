19db7e27effd19cd0fa4563604017392966a9eb3
Anonymous <xolpolatovshohjahhon5@gmail.com>
2021-08-28 12:03:20 +0500

    change file name

--- pages/ecommerce/productdetails.js
+++ pages/ecommerce/productdetails.js
@@ -1,14 +1,13 @@
 import Dashboard from "../../containers/Dashboard";
-import ProductDetailes from "../../containers/Ecomerce/productDetailes";
+import ProductDetailes from "../../containers/Ecomerce/ProductDetailes";
 import React from "react";
 
 const ProductDetailis = () => {
-    return (
-          <Dashboard menu="productdetails">
-
-        <ProductDetailes/>
+  return (
+    <Dashboard menu="productdetails">
+      <ProductDetailes />
     </Dashboard>
-    )
-}
+  );
+};
 
 export default ProductDetailis;

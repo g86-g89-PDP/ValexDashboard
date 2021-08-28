7458ee0cf09250ced138783c326b7c3a85e5e825
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2021-08-28 11:28:25 +0500

    [id]

--- pages/[id].js
+++ pages/[id].js
@@ -6,8 +6,15 @@ const Others = () => {
   const { id } = router.query;
 
   return (
-    <div>
-      <h1>Others: {id}</h1>
+    <div className="d-flex justify-content-center align-items-center mt-5">
+      <div className="container p-4 bg-dark rounded shadow">
+        <h1 className="text-warning text-center">
+          Others: <span className="text-danger">{id}</span>
+        </h1>
+        <span className="d-flex justify-content-center align-items-center">
+          <img src="/images/warning.png" className="w-25" alt="" />
+        </span>
+      </div>
     </div>
   );
 };
